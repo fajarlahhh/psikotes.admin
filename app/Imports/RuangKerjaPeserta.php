1abf90c3c71d70d63a3864f6a45231acaa9b8f5d
Andi Fajar Nugraha <andifajarlah@gmail.com>
2022-11-26 05:42:57 +0800

    baru:

--- app/Imports/RuangKerjaPeserta.php
+++ app/Imports/RuangKerjaPeserta.php
@@ -18,12 +18,13 @@ class RuangKerjaPeserta implements ToModel
   public function model(array $row)
   {
     ++$this->index;
+    $waktu = date('hms');
     return new ModelsRuangKerjaPeserta([
       'ruang_kerja_id' => $this->id,
       'nomor' => date('ymdHms') . $this->index,
       'nama' => $row[0],
-      'kata_sandi' => Hash::make(date('hms')) . $this->index,
-      'kata_sandi_show' => date('hms') . $this->index,
+      'kata_sandi' => Hash::make($waktu) . $this->index,
+      'kata_sandi_show' => $waktu . $this->index,
     ]);
   }
 }
